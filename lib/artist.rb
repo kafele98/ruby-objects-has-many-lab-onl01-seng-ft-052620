be7c5c0186d501ea8ddd3c4925ace0a57de23a99
Zoe Keller <keller.zoe98@gmail.com>
2020-06-15 12:27:48 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,12 +8,12 @@ class Artist
     @songs = []
   end
 
-  def add_song_by_name(name)
-    song = Song.new(name)
-    self.songs << song
-    song.artist = self
-    @@song_count +=1
-  end
+  # def add_song_by_name(name)
+  #   song = Song.new(name)
+  #   self.songs << song
+  #   song.artist = self
+  #   @@song_count +=1
+  # end
 
   def songs
     Song.all.select do |song|
