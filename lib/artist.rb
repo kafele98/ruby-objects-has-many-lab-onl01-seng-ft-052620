30116089021d04cb6aebf2caa54c7d873a81eabf
Zoe Keller <keller.zoe98@gmail.com>
2020-06-15 12:09:38 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,24 +3,23 @@ class Artist
   @@song_count = 0
   def initialize(name)
     @name = name
-    @songs = [] #empty collection of songs
+    @songs = [] 
   end
-
-  def songs
-    @songs
-  end
-
-  def add_song(song)
+  
+    def add_song(song)
     @songs << song
     song.artist = self
     @@song_count += 1
-  end
 
   def add_song_by_name(song_name)
     song = Song.new(song_name)
     @songs << song
     song.artist = self
     @@song_count += 1
+  end
+  
+  def songs
+    @songs
   end
 
   def self.song_count
