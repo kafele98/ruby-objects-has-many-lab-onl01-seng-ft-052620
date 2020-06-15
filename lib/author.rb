981de8e4003e6a65e8888b88f36d382ea0bc4bc0
Zoe Keller <keller.zoe98@gmail.com>
2020-06-15 12:14:03 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,31 +1,25 @@
-class Author 
+class Author
   attr_accessor :name
-  @@post_count = 0
 
   def initialize(name)
     @name = name
-    @posts = []
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title, self)
   end
 
   def posts
-    @posts
+    Post.all.select { |post| post.author == self }
   end
-
-  def add_post(post)
-    @posts << post
-    post.author = self
-    @@post_count += 1
-  end
-
-  def add_post_by_title(post_title)
-    post = Post.new(post_title)
-    @posts << post
-    post.author = self
-    @@post_count += 1
-  end
-
-  def self.post_count
-    @@post_count
-  end
-
+  
+  def posts_by_title
+    authors_posts = []
+    Post.all_by_title.each do |post|
+      if post.author == self 
+        authors_posts << post.title 
+      end  
+    end
+    authors_posts
+  end 
 end
